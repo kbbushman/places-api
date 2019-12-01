ed48a91a49098f9c2f6fafa169ef0e78e314c617
kbbushman <k.bushman@yahoo.com>
2019-11-30 23:46:35 -0800

    Initial commit, adds server boilerplate

---
+++ server.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const app = express();
+
+const placesRoutes = require('./routes/places-routes');
+
+app.use('/api/v1/places', placesRoutes);
+
+// Optional Fourth Argument In Callback Makes This An Error Handler Middleware
+app.use((error, req, res, next) => {
+  if (res.headerSent) {
+    return next(error);
+  }
+
+  res.status(error.code || 500);
+  res.json({message: error.message || 'An unknown error occurred!'});
+});
+
+app.listen(5000);
