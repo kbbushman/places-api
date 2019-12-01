4e3cee3983f373b54e88dab808efeb9080f25c06
kbbushman <k.bushman@yahoo.com>
2019-12-01 00:36:15 -0800

    Refactors routes to use places controller

--- routes/placesRoutes.js
+++ routes/placesRoutes.js
@@ -1,41 +1,9 @@
 const express = require('express');
-const HttpError = require('../models/HttpError');
+const placesController = require('../controllers/placesController');
 const router = express.Router();
 
-const TEMP_PLACES = [
-  {
-    id: 'p1',
-    title: 'Empire State Building',
-    description: 'One of the most famous sky scrapers in the world!',
-    location: {
-      lat: 40.7484474,
-      lng: -73.8971516,
-    },
-    address: '20 W 34th St, New York, NY 10001',
-    creator: 'u1',
-  },
-];
+router.get('/:pid', placesController.getPlaceById);
 
-router.get('/:pid', (req, res, next) => {
-  const placeId = req.params.pid;
-  const selectedPlace = TEMP_PLACES.find(place => place.id === placeId);
-
-  if (!selectedPlace) {
-    throw new HttpError('Could not find a place by the provided id.', 404);
-  }
-
-  res.json({status: 200, data: selectedPlace});
-});
-
-router.get('/users/:uid', (req, res, next) => {
-  const userId = req.params.uid;
-  const selectedPlace = TEMP_PLACES.find(place => place.creator === userId);
-
-  if (!selectedPlace) {
-    return next(new HttpError('Could not find a place by the provided user id.', 404));
-  }
-
-  res.json({status:200, data: selectedPlace})
-});
+router.get('/users/:uid', placesController.getPlaceByUserId);
 
 module.exports = router;
