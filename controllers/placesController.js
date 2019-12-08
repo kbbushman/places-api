e141eb8e285261b6cbde384cb752278ef69cdf18
kbbushman <k.bushman@yahoo.com>
2019-12-07 16:43:51 -0800

    Adds validation to createPlace

--- controllers/placesController.js
+++ controllers/placesController.js
@@ -1,4 +1,5 @@
 const uuid = require('uuid/v4');
+const { validationResult } = require('express-validator');
 const HttpError = require('../models/HttpError');
 
 let TEMP_PLACES = [
@@ -38,6 +39,13 @@ const getPlacesByUserId = (req, res, next) => {
 };
 
 const createPlace = (req, res, next) => {
+  const errors = validationResult(req);
+
+  if (!errors.isEmpty()) {
+    // console.log(errors);
+    throw new HttpError('Invalid inputs passed, please check your data', 422);
+  }
+
   const { title, description, coordinates, address, creator } = req.body;
   const newPlace = {
     title,
--- routes/placesRoutes.js
+++ routes/placesRoutes.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const { check } = require('express-validator');
 const placesController = require('../controllers/placesController');
 const router = express.Router();
 
@@ -8,7 +9,15 @@ router.get('/:pid', placesController.getPlaceById);
 
 router.get('/user/:uid', placesController.getPlacesByUserId);
 
-router.post('/', placesController.createPlace);
+router.post(
+  '/',
+  [
+    check('title').not().isEmpty(),
+    check('description').isLength({min: 5}),
+    check('address').not().isEmpty()
+  ],
+  placesController.createPlace
+);
 
 router.patch('/:pid', placesController.updatePlace);
 
