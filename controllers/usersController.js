4327abecfd54720ad90fc5cb0df8b3d86ac001bc
kbbushman <k.bushman@yahoo.com>
2019-12-08 22:50:31 -0800

    Adds mongoose to register

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -1,6 +1,7 @@
 const uuid = require('uuid/v4');
 const { validationResult } = require('express-validator');
 const HttpError = require('../models/HttpError');
+const User = require('../models/User');
 
 const TEMP_USERS = [
   {
@@ -15,32 +16,46 @@ const getUsers = (req, res, next) => {
   res.status(200).json({users: TEMP_USERS});
 };
 
-const signup = (req, res, next) => {
+const signup = async (req, res, next) => {
   const errors = validationResult(req);
+  let existingUser;
 
   if (!errors.isEmpty()) {
     // console.log(errors);
-    throw new HttpError('Invalid inputs passed, please check your data', 422);
+    const error = new HttpError('Invalid inputs passed, please check your data', 422);
+    return next(error);
   }
 
-  const { name, email, password } = req.body;
+  const { name, email, password, places } = req.body;
 
-  const existingUser = TEMP_USERS.find(user => user.email === email);
+  try {
+    existingUser = await User.findOne({email});
+  } catch (err) {
+    const error = new HttpError('Registration failed, please try again', 500);
+    return next(error);
+  }
 
   if (existingUser) {
-    throw new HttpError('Could not create user, email already registered', 422);
+    const error = new HttpError('User already registerd. Please login', 422);
+    return next(error);
   }
 
-  const newUser = {
-    id: uuid(),
+  const newUser = new User({
     name,
     email,
     password,
-  };
+    places,
+    image: 'https://www.google.com/url?sa=i&rct=j&q=&esrc=s&source=images&cd=&ved=2ahUKEwjih7-O-qfmAhWTGjQIHUlhDecQjRx6BAgBEAQ&url=https%3A%2F%2Fen.wikipedia.org%2Fwiki%2FFile%3APlaceholder_no_text.svg&psig=AOvVaw10W2erY6loxiwZOm8ta6OP&ust=1575959769720182',
+  });
 
-  TEMP_USERS.push(newUser);
+  try {
+    await newUser.save()
+  } catch (err) {
+    const error = new HttpError('Registration failed. Please try again', 500);
+    return next(error);
+  }
 
-  res.status(201).json({user: newUser});
+  res.status(201).json({user: newUser.toObject({getters: true})});
 };
 
 const login = (req, res, next) => {
