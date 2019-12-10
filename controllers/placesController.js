7216c6189f59ba22b855a6cf763779226785afd5
kbbushman <k.bushman@yahoo.com>
2019-12-09 19:00:40 -0800

    Adds mongoose to deletePlace

--- controllers/placesController.js
+++ controllers/placesController.js
@@ -154,14 +154,25 @@ const deletePlace = async (req, res, next) => {
   let place;
   
   try {
-    place = await Place.findById(placeId);
+    place = await Place.findById(placeId).populate('creator');
   } catch (err) {
     const error = new HttpError('Delete place failed. Please try again', 500);
     return next(error);
   }
 
+  if (!place) {
+    const error = new HttpError('Could not find place with provided id. Please try again', 500);
+    return next(error);
+  }
+
   try {
-    await place.remove();
+    // Session will prevent all transactions from succeeding if one fails
+    const sess = await mongoose.startSession();
+    sess.startTransaction();
+    await place.remove({session: sess});
+    place.creator.places.pull(place);
+    await place.creator.save({session: sess});
+    await sess.commitTransaction();
   } catch (err) {
     const error = new HttpError('Delete place failed. Please try again', 500);
     return next(error);
