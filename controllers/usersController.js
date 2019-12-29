b35bc0437205813a069d5d216739c5ee85c320ed
kbbushman <k.bushman@yahoo.com>
2019-12-28 19:00:41 -0800

    Updates default avatar image

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -44,7 +44,7 @@ const signup = async (req, res, next) => {
     email,
     password,
     places: [],
-    image: 'https://www.google.com/url?sa=i&rct=j&q=&esrc=s&source=images&cd=&ved=2ahUKEwjih7-O-qfmAhWTGjQIHUlhDecQjRx6BAgBEAQ&url=https%3A%2F%2Fen.wikipedia.org%2Fwiki%2FFile%3APlaceholder_no_text.svg&psig=AOvVaw10W2erY6loxiwZOm8ta6OP&ust=1575959769720182',
+    image: 'https://www.bsn.eu/wp-content/uploads/2016/12/user-icon-image-placeholder-300-grey.jpg',
   });
 
   try {
