ddd02f5f7c5ce41eac34e96e09ddcf888ae31a75
kbbushman <k.bushman@yahoo.com>
2019-12-28 23:43:30 -0800

    Adds multer file upload configuration for image types

---
+++ middleware/fileUpload.js
@@ -0,0 +1,28 @@
+const multer = require('multer');
+const uuid = require('uuid/v1');
+
+const MIME_TYPE_MAP = {
+  'image/png': 'png',
+  'image/jpg': 'jpg',
+  'image/jpeg': 'jpeg',
+}
+
+const fileUpload = multer({
+  limits: 500000, // value in bytes
+  storage: multer.diskStorage({
+    destination: (req, file, cb) => {
+      cb(null, 'uploads/images');
+    },
+    filename: (req, file, cb) => {
+      const ext = MIME_TYPE_MAP[file.mimetype];
+      cb(null, uuid() + '.' + ext);
+    },
+  }),
+  fileFilter: (req, file, cb) => {
+    const isValid = !!MIME_TYPE_MAP[file.mimetype];
+    const error = isValid ? null : new Error('Invalid mime type');
+    cb(error, isValid);
+  }
+});
+
+module.exports = fileUpload;
