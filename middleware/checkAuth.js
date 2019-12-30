686371627f5aa32db763aeecfca08c6e9200ce1b
kbbushman <k.bushman@yahoo.com>
2019-12-29 17:39:22 -0800

    Adds custom authentication verification middlware

---
+++ middleware/checkAuth.js
@@ -0,0 +1,24 @@
+const jwt = require('jsonwebtoken');
+const HttpError = require('../models/HttpError');
+
+require('dotenv').config();
+
+module.exports = (req, res, next) => {
+  let token;
+
+  try {
+    token = req.headers.authorization.split(' ')[1];
+
+    if (!token) {
+      throw new Error('Authentication failed. Please login and try again.')
+    }
+
+    const decodedToken = jwt.verify(token, process.env.JWT_SECRET);
+    req.userData = {userId: decodedToken.userId};
+    next();
+  } catch (err) {
+    const error = new HttpError('Authentication is required. Please login and try again', 401);
+    return next(error);
+  }
+
+}
