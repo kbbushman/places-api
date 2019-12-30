9111efd064a97ab9eec2a1ca98f63b99f832ecc1
kbbushman <k.bushman@yahoo.com>
2019-12-30 13:15:42 -0800

    Adds authorization to update and delete place

--- controllers/placesController.js
+++ controllers/placesController.js
@@ -124,6 +124,11 @@ const updatePlace = async (req, res, next) => {
     const error = new HttpError('Update place failed. Please try again', 500);
     return next(error);
   }
+
+  if (place.creator.toString() !== req.userData.userId) {
+    const error = new HttpError('You are not authorized to edit this place.', 401);
+    return next(error);
+  }
   
   place.title = title;
   place.description = description;
@@ -151,6 +156,11 @@ const deletePlace = async (req, res, next) => {
 
   if (!place) {
     const error = new HttpError('Could not find place with provided id. Please try again', 500);
+    return next(error);
+  }
+
+  if (place.creator.id !== req.userData.userId) {
+    const error = new HttpError('You are not authorized to delete this place.', 401);
     return next(error);
   }
 
