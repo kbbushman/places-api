e5eab7d37f8649f5d67ce02a897aa2d9ae3ce743
kbbushman <k.bushman@yahoo.com>
2019-12-30 20:29:45 -0800

    Heroku deployment troubleshooting

--- server.js
+++ server.js
@@ -24,16 +24,16 @@ app.use((req, res, next) => {
   next();
 });
 
-app.use((req, res, next) => {
-  console.log('REQ RECEIEVED ------------------------->')
-  next();
-})
-
 console.log('CHECK THREE... ----------------------->')
 
 app.use('/api/v1/places', placesRoutes);
 // app.use('/api/v1/users', usersRoutes);
 app.use('/api/v1/users/signup', (req, res) => res.sendStatus(201));
+
+app.use((req, res, next) => {
+  console.log('REQ RECEIEVED ------------------------->')
+  next();
+})
 
 app.use((req, res, next) => {
   const error = new HttpError('Could not find this route', 404);
