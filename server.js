02221d654ab43be13a5f410577279d10100ee17b
kbbushman <k.bushman@yahoo.com>
2019-12-30 20:14:38 -0800

    Heroku deployment troubleshooting

--- server.js
+++ server.js
@@ -23,7 +23,8 @@ app.use((req, res, next) => {
 });
 
 app.use('/api/v1/places', placesRoutes);
-app.use('/api/v1/users', usersRoutes);
+// app.use('/api/v1/users', usersRoutes);
+app.use('/api/v1/users/signup', (req, res) => res.sendStatus(201));
 
 app.use((req, res, next) => {
   const error = new HttpError('Could not find this route', 404);
