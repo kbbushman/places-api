adedfaa77886959536ec2fdba672194c14dd6e26
kbbushman <k.bushman@yahoo.com>
2019-12-30 21:10:36 -0800

    Heroku deployment troubleshooting

--- routes/usersRoutes.js
+++ routes/usersRoutes.js
@@ -8,16 +8,18 @@ const router = express.Router();
 
 router.get('/', usersController.getUsers);
 
-router.post(
-  '/signup',
-  fileUpload.single('image'),
-  [
-    check('name').not().isEmpty(),
-    check('email').normalizeEmail().isEmail(),
-    check('password').isLength({min: 6}),
-  ],
-  usersController.signup
-);
+// router.post(
+//   '/signup',
+//   fileUpload.single('image'),
+//   [
+//     check('name').not().isEmpty(),
+//     check('email').normalizeEmail().isEmail(),
+//     check('password').isLength({min: 6}),
+//   ],
+//   usersController.signup
+// );
+
+router.post('/signup', () => res.sendStatus(201));
 
 router.post('/login', usersController.login);
 
