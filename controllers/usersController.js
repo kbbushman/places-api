06d4b3e271afaae59cd1a524ad443c9c3da145e9
kbbushman <k.bushman@yahoo.com>
2019-12-30 23:20:00 -0800

    Heroku deployment troubleshooting

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -20,67 +20,67 @@ const getUsers = async (req, res, next) => {
 };
 
 const signup = async (req, res, next) => {
-  // const errors = validationResult(req);
-  // let existingUser;
+  const errors = validationResult(req);
+  let existingUser;
 
-  // if (!errors.isEmpty()) {
-  //   // console.log(errors);
-  //   const error = new HttpError('Invalid inputs passed, please check your data', 422);
-  //   return next(error);
-  // }
+  if (!errors.isEmpty()) {
+    // console.log(errors);
+    const error = new HttpError('Invalid inputs passed, please check your data', 422);
+    return next(error);
+  }
 
-  // const { name, email, password } = req.body;
+  const { name, email, password } = req.body;
 
-  // try {
-  //   existingUser = await User.findOne({email});
-  // } catch (err) {
-  //   const error = new HttpError('Registration failed, please try again', 500);
-  //   return next(error);
-  // }
+  try {
+    existingUser = await User.findOne({email});
+  } catch (err) {
+    const error = new HttpError('Registration failed, please try again', 500);
+    return next(error);
+  }
 
-  // if (existingUser) {
-  //   const error = new HttpError('User already registered. Please login', 422);
-  //   return next(error);
-  // }
+  if (existingUser) {
+    const error = new HttpError('User already registered. Please login', 422);
+    return next(error);
+  }
 
-  // let hashedPassword;
-  // try {
-  //   hashedPassword = await bcrypt.hash(password, 12);
-  // } catch (err) {
-  //   const error = new HttpError('Could not create account, please try again.', 500);
-  //   return next(error);
-  // }
+  let hashedPassword;
+  try {
+    hashedPassword = await bcrypt.hash(password, 12);
+  } catch (err) {
+    const error = new HttpError('Could not create account, please try again.', 500);
+    return next(error);
+  }
 
-  // const newUser = new User({
-  //   name,
-  //   email,
-  //   password: hashedPassword,
-  //   places: [],
-  //   image: req.file.path,
-  //   // image: 'https://www.bsn.eu/wp-content/uploads/2016/12/user-icon-image-placeholder-300-grey.jpg',
-  // });
+  const newUser = new User({
+    name,
+    email,
+    password: hashedPassword,
+    places: [],
+    image: req.file.path,
+    // image: 'https://www.bsn.eu/wp-content/uploads/2016/12/user-icon-image-placeholder-300-grey.jpg',
+  });
 
-  // try {
-  //   await newUser.save()
-  // } catch (err) {
-  //   const error = new HttpError('Registration failed. Please try again', 500);
-  //   return next(error);
-  // }
+  try {
+    await newUser.save()
+  } catch (err) {
+    const error = new HttpError('Registration failed. Please try again', 500);
+    return next(error);
+  }
 
-  // let token;
-  // try {
-  //   token = jwt.sign(
-  //     {userId: newUser.id, email: newUser.email},
-  //     process.env.JWT_SECRET,
-  //     {expiresIn: '1h'}
-  //   );
-  // } catch (err) {
-  //   const error = new HttpError('Registration failed. Please try again', 500);
-  //   return next(error);
-  // }
+  let token;
+  try {
+    token = jwt.sign(
+      {userId: newUser.id, email: newUser.email},
+      process.env.JWT_SECRET,
+      {expiresIn: '1h'}
+    );
+  } catch (err) {
+    const error = new HttpError('Registration failed. Please try again', 500);
+    return next(error);
+  }
 
-  // res.status(201).json({userId: newUser.id, email: newUser.email, token: token});
-  res.sendStatus(201);
+  res.status(201).json({userId: newUser.id, email: newUser.email, token: token});
+  // res.sendStatus(201);
 };
 
 const login = async (req, res, next) => {
