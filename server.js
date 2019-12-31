a50867e4ea8b4cf5b9bd84ce9d3ea55c33629d99
kbbushman <k.bushman@yahoo.com>
2019-12-30 20:26:22 -0800

    Heroku deployment troubleshooting

--- server.js
+++ server.js
@@ -18,16 +18,16 @@ const usersRoutes = require('./routes/usersRoutes');
 console.log('CHECK TWO... ----------------------->')
 
 app.use((req, res, next) => {
-  console.log('REQ RECEIEVED ------------------------->')
-  next();
-})
-
-app.use((req, res, next) => {
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PATCH, DELETE');
   next();
 });
+
+app.use((req, res, next) => {
+  console.log('REQ RECEIEVED ------------------------->')
+  next();
+})
 
 console.log('CHECK THREE... ----------------------->')
 
